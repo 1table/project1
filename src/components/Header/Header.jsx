79c923164fc52b6a589a8f485f9b42fe34291b6b
unknown <wearw133@outlook.com>
2024-05-04 19:30:22 +0600

    header is ready

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,12 +4,14 @@ const Header = () => {
   return (
     <div className='header'>
       <div className="container">
+        <div className="main-block">
         <a href="" className="logo">logo</a>
         <nav className="nav_link">
             <a href="#" className="nav_link-item">Главная</a>
             <a href="#" className="nav_link-item">О нас</a>
             <a href="#" className="nav_link-item">Контакты</a>
         </nav>
+        </div>
       </div>
     </div>
   )
